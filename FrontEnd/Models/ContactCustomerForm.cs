055b089105143acb19ab59151b38b843e495318c
t-kasparaitis <tkasparaitis@gmail.com>
2022-05-23 17:55:24 -0400

    Validation updates

---
+++ FrontEnd/Models/ContactCustomerForm.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd.Models
+{
+    public class ContactCustomerForm
+    {
+        [Required]
+        [EmailAddress]
+        public string Recipient { get; set; } = String.Empty;
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "A subject is required.")]
+        public string Subject { get; set; } = String.Empty;
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(10000, MinimumLength = 2, ErrorMessage = "A message is required.")] // String Max Length here might be a consideration for abuse/data storage
+        public string Body { get; set; } = String.Empty;
+    }
+}
--- FrontEnd/Pages/ContactCustomer.cshtml.cs
+++ FrontEnd/Pages/ContactCustomer.cshtml.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
+using FrontEnd.Models;
 
 namespace FrontEnd.Pages
 {
@@ -13,38 +13,31 @@ namespace FrontEnd.Pages
             _client = client;
             _logger = logger;
         }
+        public ContactCustomerForm ContactCustomerForm { get; set; }
         
         public void OnGet()
         {
         }
 
-        [Required]
-        [EmailAddress]
-        public string Recipient { get; set; } = String.Empty;
-        [Required]
-        [StringLength(255, MinimumLength = 2)]
-        public string Subject { get; set; } = String.Empty;
-        [Required]
-        [StringLength(10000, MinimumLength = 2)] // String Max Length here might be a consideration for abuse/data storage
-        public string Body { get; set; } = String.Empty;
-
         // note with 1 server hosting front-end and 2nd server hosting front-end, this is in essence a 3rd party API call
         // trying to use AJAX would make the call result in redirection as the call is not from the same origin
-        public IActionResult OnPostAsync()
+        // our API will log to the DB, however if the API is down info won't be captured - for such logging additional logic would be required
+        public IActionResult OnPostAsync(ContactCustomerForm contactCustomerForm)
         {
-            // create a dictionary (unique key-value pairs) for our user e-mail input
-            var content = new Dictionary<string, string>
+            if (ModelState.IsValid) 
             {
-                {"recipient", Request.Form[nameof(Recipient)]},
-                {"subject", Request.Form[nameof(Subject)]},
-                {"body", Request.Form[nameof(Body)]}
-            };
+                // create a dictionary (unique key-value pairs) for our user e-mail input
+                var content = new Dictionary<string, string>
+                {
+                    {"recipient", contactCustomerForm.Recipient},
+                    {"subject", contactCustomerForm.Subject},
+                    {"body", contactCustomerForm.Body}
+                };
 
-            // use Singleton-created http client to make 3rd party API call + convert dictionary of user input
-            // PostAsync will run in the background, but we don't wait for results which allows return Page() to give the illusion of immediate response!
-            _client.PostAsync("http://localhost:5000/Email", new FormUrlEncodedContent(content));
-            
-            // our API will log to the DB, however if the API is down info won't be captured - for such logging additional logic would be required
+                // use Singleton-created http client to make 3rd party API call + convert dictionary of user input
+                // PostAsync will run in the background, but we don't wait for results which allows return Page() to give the illusion of immediate response!
+                _client.PostAsync("http://localhost:5000/Email", new FormUrlEncodedContent(content));
+            }
 
             return Page();
         }
